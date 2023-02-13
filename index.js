249b720a11e3b0a664d10aaadefdea0ad20311f6
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-02-13 20:06:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let miNombre = "Federico";
+let miApellido = "Faccipieri";
+let miEdad = 27;
+let miMascota = "Blanquita";
+let edadMascota = 12;
+let soyArgentino = true;
+
+console.log(miNombre);
+console.log(miApellido);
+console.log(miEdad);
+console.log(miMascota);
+console.log(soyArgentino);
+
+let nombreCompleto = miApellido + " " + miNombre;
+
+// let textoPresentacion = CHEQUEARRRRRRRRRRRRRRR
+
+let sumaEdades = miEdad + edadMascota;
+let restaEdades = miEdad - edadMascota;
+let productoEdades = miEdad * edadMascota;
+let divisionEdades = miEdad / edadMascota;
+
+let alumno = {
+    nombre: "Jorge",
+    apellido: "Rodriguez",
+    edad: 46,
+    profesion: "Backend developer",
+    nacionalidad: "Argentino",
+}
+
+console.table(alumno);
+
+console.log(alumno.nombre);
+console.log(alumno.apellido);
+console.log(alumno.edad);
+console.log(alumno.nacionalidad);
+console.log(alumno.profesion);
+
+let frutas = ["Belgrano", "River", "Boca"];
